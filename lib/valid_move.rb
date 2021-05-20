7d10b81fb9b9c69056d1601c5ad5f09ac2b036b6
Chris Suarez <chrissuarez001@gmail.com>
2021-05-20 19:58:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 def valid_move?(board , index)
 if board[index] == " " || board[index] == "" ||
+return true 
 end
-end 
+
+
 def position_taken? (board , index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
